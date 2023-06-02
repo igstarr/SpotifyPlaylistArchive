75d35e6b91be9611e7a75fae04f83d197bfd52bd
Fredrik Sjöman <fredrik.sjoman@fujitsu.com>
2023-06-02 14:39:53 +0200

    New time

--- GetNewSong/Initialize.cs
+++ GetNewSong/Initialize.cs
@@ -21,7 +21,7 @@ namespace GetNewSong
     {
         [FunctionName("UpdateSpotifyArchiveList")]
         public static async Task Run(
-            [TimerTrigger("0 0 * * *")]TimerInfo myTimer, ExecutionContext context, ILogger log)
+            [TimerTrigger("45 14 * * *")]TimerInfo myTimer, ExecutionContext context, ILogger log)
         {
             var config = new ConfigurationBuilder()
                 .SetBasePath(context.FunctionAppDirectory)
