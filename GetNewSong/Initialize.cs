c2b3c2c14bcfbb4a16dab87b82a112d734a1f5f5
Fredrik Sjöman <fredrik.sjoman@fujitsu.com>
2023-06-07 09:22:33 +0200

    Update timer and removed stuff

--- GetNewSong/Initialize.cs
+++ GetNewSong/Initialize.cs
@@ -21,7 +21,7 @@ namespace GetNewSong
     {
         [FunctionName("UpdateSpotifyArchiveList")]
         public static async Task Run(
-            [TimerTrigger("0 6 * * *")]TimerInfo myTimer, ExecutionContext context, ILogger log)
+            [TimerTrigger("0 45 9 * * *")]TimerInfo myTimer, ExecutionContext context, ILogger log)
         {
             var config = new ConfigurationBuilder()
                 .SetBasePath(context.FunctionAppDirectory)
