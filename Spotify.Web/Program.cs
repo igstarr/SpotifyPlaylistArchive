f054155d6bffa4652949af0fa2fa4f7ca0a25ca1
Fredrik S <fredrik.sjoman@gmail.com>
2023-10-23 16:05:48 +0200

    Added editorconfig and fixed files

--- Spotify.Web/Program.cs
+++ Spotify.Web/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Spotify.Web;
 using Spotify.Web.Data;
@@ -16,9 +16,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
